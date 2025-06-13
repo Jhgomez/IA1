300cf408575ac2c9f2c364b11b8b5618931a4604
Juan Gomez <juan.e.g.f@outlook.com>
2025-06-12 23:56:59 -0600

    chore: configure first endpoint

--- Lab/project1/backend/api.go
+++ Lab/project1/backend/api.go
@@ -1,19 +1,16 @@
 package main
 
 import (
-    "encoding/json"
+  //  "encoding/json"
     "context"
     "net/http"
     "fmt"
-    "bytes"
-    "log"
+  //  "bytes"
+  //  "log"
     "os"
-    "io"
+   // "io"
 
     "github.com/gin-gonic/gin"
-
-    "google.golang.org/grpc"
-    "google.golang.org/grpc/credentials/insecure"
 )
 
 type courserecord struct {
@@ -24,11 +21,10 @@ type courserecord struct {
 }
 
 var (
-    opts = []grpc.DialOption{ grpc.WithTransportCredentials(insecure.NewCredentials()) }
     grpcClientUrl = fmt.Sprintf("%s:%s", os.Getenv("GRPC_CLIENT_HOST"), os.Getenv("GRPC_CLIENT_PORT"))
-    grpcServerUrl = fmt.Sprintf("%s:%s", os.Getenv("GRPC_SERVER_HOST"), os.Getenv("GRPC_SERVER_PORT"))
+//    grpcServerUrl = fmt.Sprintf("%s:%s", os.Getenv("GRPC_SERVER_HOST"), os.Getenv("GRPC_SERVER_PORT"))
     ctx = context.Background()
-    rustServerUrl = fmt.Sprintf("http://%s:%s", os.Getenv("RUST_SERVER_HOST"), os.Getenv("RUST_SERVER_PORT"))
+//    rustServerUrl = fmt.Sprintf("http://%s:%s", os.Getenv("RUST_SERVER_HOST"), os.Getenv("RUST_SERVER_PORT"))
 )
 
 func allGood(c *gin.Context) {
@@ -45,63 +41,8 @@ func postCourse(c *gin.Context) {
         return
     }
 
-    conn, connErr := grpc.NewClient(grpcServerUrl, opts...)
-
-    if connErr != nil {
-        log.Fatalf("fail to dial: %v", connErr)
-        c.String(http.StatusBadRequest, connErr.Error())
-        return
-    }
-
-    defer conn.Close()
     
-    client := pb.NewCourseClient(conn)
-
-    log.Println("gRPC client connected to server", grpcServerUrl)
-
-   
-
-    response, responseErr := client.PostCourse(ctx, &pb.CourseRecord{ 
-        Curso: courseRecord.Curso,
-        Facultad: courseRecord.Facultad,
-        Carrera: courseRecord.Carrera,
-        Region: courseRecord.Region,
-    })
-
-    if responseErr != nil {
-        log.Fatalf("gRPC post failed: %v", responseErr)
-        c.String(http.StatusBadRequest, responseErr.Error())
-        return
-    }
-
-    courseJson, courseJsonErr := json.Marshal(courseRecord)
-
-    if courseJsonErr != nil {
-        log.Fatalf("parsing back to Json failed: %v", courseJsonErr)
-        c.String(http.StatusBadRequest, courseJsonErr.Error())
-        return
-    }
-
-    rustResponse, rustErr := http.Post(fmt.Sprintf("%s/course", rustServerUrl), "application/json", bytes.NewBuffer(courseJson) )
-
-    if rustErr != nil {
-        log.Fatalf("post to Rust server failed: %v", rustErr)
-        c.String(http.StatusBadRequest, rustErr.Error())
-        return
-    }
-
-    defer rustResponse.Body.Close()
-    rustBody, rustBodyErr := io.ReadAll(rustResponse.Body)
-
-    if rustBodyErr != nil {
-        log.Fatalf("Rust response body failure: %v", rustBodyErr)
-        c.String(http.StatusBadRequest, rustBodyErr.Error())
-        return
-    }
-
-    success := fmt.Sprintf("gRPC server response: %s, Rust REST server response: %s", response.Response, string(rustBody))
-
-    c.String(http.StatusOK, success)
+    c.String(http.StatusOK, "ok")
 
 }
 
@@ -112,3 +53,6 @@ func main() {
 
     router.Run(grpcClientUrl)
 }
+
+// export GRPC_CLIENT_PORT=8000 \
+// export GRPC_CLIENT_HOST=localhost
