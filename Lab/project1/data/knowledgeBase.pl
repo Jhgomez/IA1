b1073c0de7d7ac5d6a106c22851159d1b904af90
Juan Gomez <juan.e.g.f@outlook.com>
2025-06-14 14:11:30 -0600

    chore: add first knowledge base version

---
+++ Lab/project1/data/knowledgeBase.pl
@@ -0,0 +1,29 @@
+% Career Fact: career(Faculty, Career, Aptitude, Skill, Interest)
+
+% Engineering
+career(ingenieria, quimica, analisis, laboratorio, procesos_industriales).
+career(ingenieria, electricta, razonamiento, circuitos, energia).
+career(ingenieria, mecanica, resolucion_de_problemas, diseno_mecanico, maquinaria).
+career(ingenieria, sistemas, logica, programacion, tecnologia).
+career(ingenieria, civil, matematica, dibujo, construccion).
+
+% Medicine
+career(medicina, medicina_general, biologia, empatia, salud).
+career(medicina, nutricion, biologia, comunicacion, bienestar).
+career(medicina, enfermeria, empatia, atencion, salud_publica).
+
+% Social Sciences and Humanities
+career(humanidades, filosofia, analisis, redaccion, lectura).
+career(humanidades, psicologia, observacion, escucha_activa, comportamiento_humano).
+career(humanidades, sociologia, pensamiento_critico, investigacion, justicia_social).
+
+% Economic Sciences
+career(economica, administracion, liderazgo, orgnaizacion, negocios).
+career(economica, economia, analisis_cuantitativo, investigacion, mercados).
+career(economica, contaduria, precision, numeros, gestion_financiera).
+
+% Judgement Sciences and Socials
+career(derecho, derecho, argumentacion, lectura_comprensiva, justicia).
+
+% Design and Architecture
+career(arquitectura, arquitectura, creatividad, diseno_espacial, urbanizacion).
