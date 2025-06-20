a23b9735402243696b4f723cf3d61a7d4fb0a70c
Juan Gomez <juan.e.g.f@outlook.com>
2025-06-20 15:15:44 -0600

    chore: call main screen

--- Lab/project1/client/presentation/features/home/home.go
+++ Lab/project1/client/presentation/features/home/home.go
@@ -1,2 +1,52 @@
 package home
 
+import (
+	// "image/color"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/theme"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/layout"
+	"fyne.io/fyne/v2/canvas"
+	"fyne.io/fyne/v2/widget"
+)
+
+func HomeScreen() fyne.CanvasObject {
+	// Title
+	title := canvas.NewText("Unimatch", theme.PrimaryColor())
+	title.TextStyle = fyne.TextStyle{Bold: true}
+	title.Alignment = fyne.TextAlignCenter
+	title.TextSize = 28 // default is 14
+
+	// // Title
+	// title := widget.NewLabelWithStyle("Career Path Assistant", fyne.TextAlignCenter, fyne.TextStyle{Bold: true})
+
+	// Description
+	description := widget.NewLabelWithStyle(
+	"Bienvenido, Unimatch es un sistema inteligente que te ayudara a encontrar carreras\nafines a tus apitudes, habilidades e intereses",
+		fyne.TextAlignCenter,
+		fyne.TextStyle{},
+	)
+
+	// Buttons
+	studentBtn := widget.NewButtonWithIcon("Student", theme.AccountIcon(), func() {
+		// Handle student button click
+	})
+	adminBtn := widget.NewButtonWithIcon("Administrator", theme.SettingsIcon(), func() {
+		// Handle admin button click
+	})
+
+	buttons := container.NewHBox(layout.NewSpacer(), studentBtn, layout.NewSpacer(), adminBtn, layout.NewSpacer())
+
+	// Layout: VBox (Title > Description > Buttons)
+	content := container.NewVBox(
+		layout.NewSpacer(),
+		title,
+		description,
+		layout.NewSpacer(),
+		buttons,
+		layout.NewSpacer(),
+	)
+
+	return content
+}
--- Lab/project1/client/unimatch.go
+++ Lab/project1/client/unimatch.go
@@ -1,21 +1,65 @@
-package uns
+package main
 
-// import (
-// 	"fyne.io/fyne/v2/container"
-// 	"fyne.io/fyne/v2/layout"
-// 	"fyne.io/fyne/v2/widget"
+import (
+	"fmt"
 
-// 	"unimatch/presentation/navigation"
-// )
+	"fyne.io/fyne/v2"
+	// "fyne.io/fyne/v2/layout"
+	// "fyne.io/fyne/v2/widget"
 
-// func main() {
-// 	// messages send button
-// 	sendButton := widget.NewButton("Enviar", func() {
-// 		navigation.NavigateWithNewWindow("new", widget.NewLabel("New Window"))
-// 	})
+	"unimatch/presentation/navigation"
+	"unimatch/presentation/features/home"
+	"unimatch/data/repository/knowledge"
+)
 
-// 	// Main content layout
-// 	home := container.NewVBox(sendButton,layout.NewSpacer())
+
+func main() {
+	_, err := knowledgerepo.GetKnowledgeRepo().LoadKnowledgeBase()
+
+	if err != nil {
+		fmt.Printf("error in knowledge repo %v", err)
+	}
+	
+	navigation.NavigateWithNewWindow(
+		"Unimatch",  //windowTitle
+		home.HomeScreen(),  //content
+		false, // shouldHide
+		fyne.NewSize(400, 500), // windows size
+		nil, // onClose
+	)
+
+	// // messages send button
+	// sendButton := widget.NewButton("Enviar", func() {
+	// 	navigation.NavigateWithNewWindow(
+	// 		"new", //windowTitle
+	// 		widget.NewLabel("New Window"), //content
+	// 		true,	// shouldHide
+	// 		func() {	// onClose
+	// 			// navigation.PopBackstack()
+	// 		},
+	// 	)
+	// })
+
+	// // sendButton := widget.NewButton("Enviar", func() {
+	// // 	navigation.NavigateWithCurrentWindow(
+	// // 		"new", //windowTitle
+	// //		fyne.NewSize(400, 300), // size
+	// // 		widget.NewLabel("New Window"), //content
+	// // 		// true,	// shouldHide
+	// // 		// func() {	// onClose
+	// // 		// 	navigation.PopBackstack()
+	// // 		// },
+	// // 	)
+	// // })
+
+	// // Main content layout
+	// home := container.NewVBox(sendButton,layout.NewSpacer())
 		
-// 	navigation.SetUpHomeScreen("master", home)
-// }
+	// navigation.NavigateWithNewWindow(
+	// 	"home",  //windowTitle
+	// 	home,  //content
+	// 	false, // shouldHide
+	//	fyne.NewSize(400, 300), // size
+	// 	nil, // onClose
+	// )
+}
