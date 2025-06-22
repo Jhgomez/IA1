5d9b44c6b5fc85771705cac1d16460767d3de5ce
Juan Gomez <juan.e.g.f@outlook.com>
2025-06-22 00:16:03 -0600

    chore: remove spacing

--- Lab/project1/client/data/knowledge/knowledgeBase.pl
+++ Lab/project1/client/data/knowledge/knowledgeBase.pl
@@ -1,32 +1,32 @@
 % Career Fact: career(Faculty, Career, Aptitude, Skill, Interest)
 
 % Engineering
-career(ingenieria, quimica, analisis, laboratorio, procesos_industriales).
+career(ingenieria, quimica, analisis, laboratorio, procesos industriales).
 career(ingenieria, electricta, razonamiento, circuitos, energia).
-career(ingenieria, mecanica, resolucion_de_problemas, diseno_mecanico, maquinaria).
+career(ingenieria, mecanica, resolucion de problemas, diseno mecanico, maquinaria).
 career(ingenieria, sistemas, logica, programacion, tecnologia).
 career(ingenieria, civil, matematica, dibujo, construccion).
 
 % Medicine
-career(medicina, medicina_general, biologia, empatia, salud).
+career(medicina, medicina general, biologia, empatia, salud).
 career(medicina, nutricion, biologia, comunicacion, bienestar).
-career(medicina, enfermeria, empatia, atencion, salud_publica).
+career(medicina, enfermeria, empatia, atencion, salud publica).
 
 % Social Sciences and Humanities
 career(humanidades, filosofia, analisis, redaccion, lectura).
-career(humanidades, psicologia, observacion, escucha_activa, comportamiento_humano).
-career(humanidades, sociologia, pensamiento_critico, investigacion, justicia_social).
+career(humanidades, psicologia, observacion, escucha activa, comportamiento humano).
+career(humanidades, sociologia, pensamiento critico, investigacion, justicia social).
 
 % Economic Sciences
 career(economica, administracion, liderazgo, orgnaizacion, negocios).
-career(economica, economia, analisis_cuantitativo, investigacion, mercados).
-career(economica, contaduria, precision, numeros, gestion_financiera).
+career(economica, economia, analisis cuantitativo, investigacion, mercados).
+career(economica, contaduria, precision, numeros, gestion financiera).
 
 % Judgement Sciences and Socials
-career(derecho, derecho, argumentacion, lectura_comprensiva, justicia).
+career(derecho, derecho, argumentacion, lectura comprensiva, justicia).
 
 % Design and Architecture
-career(arquitectura, arquitectura, creatividad, diseno_espacial, urbanizacion).
+career(arquitectura, arquitectura, creatividad, diseno espacial, urbanizacion).
 
 % Rules
-suggested_career(Faculty, Career, Aptitude, Skill, Interest) :- career(Faculty, Career, Aptitude, Skill, Interest).
+suggested career(Faculty, Career, Aptitude, Skill, Interest) :- career(Faculty, Career, Aptitude, Skill, Interest).
